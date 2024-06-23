f37567c3ff1df5b70170190a0ad48ce4dc6ee561
txxxxc <tomoya113.code@gmail.com>
2024-06-23 13:43:53 +0900

    update: main.go

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -637,8 +637,8 @@ func postIsu(c echo.Context) error {
 	defer tx.Rollback()
 
 	_, err = tx.Exec("INSERT INTO `isu`"+
-		"	(`jia_isu_uuid`, `name`, `image`, `jia_user_id`) VALUES (?, ?, ?, ?)",
-		jiaIsuUUID, isuName, img, jiaUserID)
+		"	(`jia_isu_uuid`, `name`, `jia_user_id`) VALUES (?, ?, ?)",
+		jiaIsuUUID, isuName, jiaUserID)
 	if err != nil {
 		mysqlErr, ok := err.(*mysql.MySQLError)
 
